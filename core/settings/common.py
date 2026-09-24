09708911e1d92e18cedc25c113e2e8bc5e630b56
Watumwa <watumwaizaac32@gmail.com>
2026-09-24 12:38:30 +0300

    Fix Unfold admin color palette

--- core/settings/common.py
+++ core/settings/common.py
@@ -323,7 +323,19 @@ UNFOLD = {
     "SITE_SYMBOL": "school",
     "SHOW_LANGUAGES": False,
     "COLORS": {
-        "primary": "emerald",
+        "primary": {
+            "50": "#ecfdf5",
+            "100": "#d1fae5",
+            "200": "#a7f3d0",
+            "300": "#6ee7b7",
+            "400": "#34d399",
+            "500": "#10b981",
+            "600": "#059669",
+            "700": "#047857",
+            "800": "#065f46",
+            "900": "#064e3b",
+            "950": "#022c22",
+        },
     },
     "SIDEBAR": {
         "show_search": True,
