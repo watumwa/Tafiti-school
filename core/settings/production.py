d106b96352b72ef89c245780069233f8f088ab67
Watumwa <watumwaizaac32@gmail.com>
2026-09-24 13:06:07 +0300

    Use writable media path on Vercel

--- core/settings/production.py
+++ core/settings/production.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/tmp/tafiti-media'
